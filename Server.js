6dfcefa95e5758e16415842601d4a7993a6efc4d
Resham <reshamupadhyay48@gmail.com>
2025-01-06 22:55:52 +0530

    Added get request

--- Server.js
+++ Server.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
+app.get("/goals", (req, res) => {
+  const goals = JSON.parse(fs.readFileSync("data.json"));
+  res.json(goals);
+});
 
 
-app.listen(3000)
+app.listen(3000);
