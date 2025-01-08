79552b4b60cd9f12da03b84c841aeaab8e3c6aae
Resham <reshamupadhyay48@gmail.com>
2025-01-08 23:00:29 +0530

    added basic server methods

--- Server.js
+++ Server.js
@@ -2,10 +2,43 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
-app.get("/goals", (req, res) => {
+app.use(express.json());
+
+app.get("/goals", function(req, res){
   const goals = JSON.parse(fs.readFileSync("data.json"));
   res.json(goals);
 });
 
+app.get("/goals/:id", function(req, res){
+  const goals = JSON.parse(fs.readFileSync("data.json"));
+  const goalId = parseInt(req.params.id);
+  const goal = goals.find(g => g.id === goalId);
+
+  if(goal){
+    res.json(goal);
+  } else {
+    res.status(404).json({msg : "goal not found"})
+  }
+});
+
+// add
+app.post("/goals", function(req, res){
+
+})
+
+// update
+app.put("/goals/:id", function(req, res) {
+  const goals = JSON.parse(fs.readFileSync("data.json"));
+  const goalId = parseInt(req.params.id);
+  const goal = goals.find(g => g.id === goalId);
+
+  fs.writeFile("data.json", content, (err) => {
+    
+  })
+})
+
+app.all("*", (req, res) => {
+  res.status(404).send("Route not found");
+});
 
 app.listen(3000);
