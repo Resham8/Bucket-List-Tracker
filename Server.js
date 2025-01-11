77c41784649e21dad229ba10b3f7d49d5fd57609
Resham <reshamupadhyay48@gmail.com>
2025-01-11 21:36:54 +0530

    Added put and delete methods
    made changes to helper functions as well.

--- Server.js
+++ Server.js
@@ -1,78 +1,128 @@
 const express = require("express");
 const fs = require("fs");
-const { json } = require("stream/consumers");
 const app = express();
 
 app.use(express.json());
 
+const DATA_FILE = "./data.json";
 let idCounter = 1;
-const path = './data.json';
 
-let fileData = []
-
-function getData(){
-  fs.readFile(path, 'utf8', (err, data) => {
-    if(err){
-      console.log(err);      
+function readDataSync() {
+  try {
+    if (fs.existsSync(DATA_FILE)) {
+      const data = fs.readFileSync(DATA_FILE, "utf8");
+      return JSON.parse(data);
     }
-    const fileData = data;    
-  })
-  return fileData;
+    return [];
+  } catch (error) {
+    console.error("Error reading file:", error);
+    return [];
+  }
 }
 
-app.get("/goals", function (req, res) {
-  const goals = getData();
-  if(goals.length > 0){
-    res.json(getData());
-  } else {
-    res.status(204).json({ msg: "List is empty" }); // 204 is for no content
+function writeDataSync(data) {
+  try {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2));
+    return true;
+  } catch (error) {
+    console.error("Error writing file:", error);
+    return false;
   }
-  
+}
+
+// GET all goals
+app.get("/goals", (req, res) => {
+  const goals = readDataSync();
+  if (goals.length > 0) {
+    res.json(goals);
+  } else {
+    res.status(204).json({ msg: "List is empty" });
+  }
 });
 
-app.get("/goals/:id", function (req, res) {
-  const goals = JSON.parse(fs.readFileSync(path));
+// GET single goal
+app.get("/goals/:id", (req, res) => {
+  const goals = readDataSync();
   const goalId = parseInt(req.params.id);
   const goal = goals.find((g) => g.id === goalId);
 
   if (goal) {
     res.json(goal);
   } else {
-    res.status(404).json({ msg: "goal not found" });
+    res.status(404).json({ msg: "Goal not found" });
   }
 });
 
-
-app.post("/goals", function (req, res) {
+// POST new goal
+app.post("/goals", (req, res) => {
   const { title, description, isCompleted } = req.body;
-  let newGoal = {
-    id: idCounter++,
+  const goals = readDataSync();
+
+  // Find the highest ID and increment
+  idCounter = Math.max(0, ...goals.map((g) => g.id)) + 1;
+
+  const newGoal = {
+    id: idCounter,
     title,
     description,
     isCompleted: isCompleted || false,
   };
 
-  let d = getData();
+  goals.push(newGoal);
 
-  d.push(newGoal);
-
-  const jsonString = JSON.stringify(d, null, 2);
-  fs.writeFile(path, jsonString, (err) => {
-    if (err) {
-      res.status(404).json({ msg: "Error" });
-    } else {
-      res.status(200).json({ id: newGoal.id });        
-    }
-  });  
+  if (writeDataSync(goals)) {
+    res.status(201).json({ id: newGoal.id });
+  } else {
+    res.status(500).json({ msg: "Error saving goal" });
+  }
 });
 
-// update
-app.put("/goals/:id", function (req, res) {
- 
+// PUT update goal
+app.put("/goals/:id", (req, res) => {
+  const goalId = parseInt(req.params.id);
+  const { title, description, isCompleted } = req.body;
+  
+  const goals = readDataSync();
+
+  const goalIndex = goals.findIndex((g) => g.id === goalId);
+
+  if (goalIndex === -1) {
+    return res.status(404).json({ msg: "Goal not found" });
+  }
+
+  goals[goalIndex] = {
+    ...goals[goalIndex],
+    title: title || goals[goalIndex].title,
+    description: description || goals[goalIndex].description,
+    isCompleted:
+      isCompleted !== undefined ? isCompleted : goals[goalIndex].isCompleted,
+  };
+
+  if (writeDataSync(goals)) {
+    res.json({ msg: "Goal updated successfully" });
+  } else {
+    res.status(500).json({ msg: "Error updating goal" });
+  }
+});
+
+app.delete("/goals/:id", function (req, res) {
+  const goals = readDataSync();
+
+  const goalIndex = goals.findIndex((g) => g.id === parseInt(req.params.id));
+  if (goalIndex !== -1) {
+    goals.splice(goalIndex, 1);
+    // writeDataSync(data);
+    if (writeDataSync(goals)) {
+      res.json({ msg: "Deleted" });
+    } else {
+      res.status(404).json({ error: "Todo Id not found" });
+    }
+  }
 });
 
 app.all("*", (req, res) => {
   res.status(404).send("Route not found");
 });
+
 
 app.listen(3000);
